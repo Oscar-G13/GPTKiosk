ca39c6cbc8c480c1981772a081a1c0f0bd1ff534
OscorpIndustriesio <oscar.garcia5592@my.riohondo.edu>
2023-03-23 01:11:56 -0700

    Update chatgpt.js
    This updated JavaScript code stores the user's input and the AI's response in a conversationHistory array, which is then used to provide context for future responses. The AI's prompt text is updated to include the previous inputs and outputs in the conversationHistory.

--- chatgpt.js
+++ chatgpt.js
@@ -1,28 +1,44 @@
+let conversationHistory = [];
+
 document.getElementById("promptForm").addEventListener("submit", async (e) => {
     e.preventDefault();
     const prompt = document.getElementById("prompt").value;
-    const responseElement = document.getElementById("response");
-    responseElement.textContent = "Processing...";
+    const responseContainer = document.getElementById("responseContainer");
+    const responseElement = document.createElement("p");
+    responseContainer.appendChild(responseElement);
+    responseElement.textContent = "> " + prompt;
 
     try {
-        const result = await fetchChatGPT(prompt);
+        const result = await fetchChatGPT(prompt, conversationHistory);
         const responseText = result.choices[0].text.trim();
-        typeText(responseElement, responseText, 50);
+        conversationHistory.push({ prompt, responseText });
+        typeText(responseElement, "< " + responseText, 50);
     } catch (error) {
         responseElement.textContent = "An error occurred. Please try again.";
     }
+
+    document.getElementById("prompt").value = "";
 });
 
-async function fetchChatGPT(prompt) {
+async function fetchChatGPT(prompt, conversationHistory) {
     const apiKey = "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";
-    const apiUrl = "https://api.openai.com/v1/engines/gpt-3.5-turbo/completions"; // Changed to the gpt-3.5-turbo model
+    const apiUrl = "https://api.openai.com/v1/engines/gpt-3.5-turbo/completions";
     const headers = {
         "Content-Type": "application/json",
         "Authorization": `Bearer ${apiKey}`
     };
 
+    let promptText = `You are an AI language model. Please provide a plain English response to the following question: "${prompt}". Your response should be clear, concise, and easy to understand, without using any code or technical jargon. Thank you!`;
+
+    if (conversationHistory.length > 0) {
+        promptText += " Previous inputs and outputs:\n";
+        conversationHistory.forEach((item) => {
+            promptText += `\nInput: ${item.prompt}\nOutput: ${item.responseText}\n`;
+        });
+    }
+
     const data = {
-        "prompt": `You are an AI language model. Please provide a plain English response to the following question: "${prompt}". Your response should be clear, concise, and easy to understand, without using any code or technical jargon. Thank you!`,
+        "prompt": promptText,
         "max_tokens": 500,
         "n": 1,
         "stop": null,
@@ -43,12 +59,11 @@ async function fetchChatGPT(prompt) {
 }
 
 function typeText(element, text, speed) {
-    element.textContent = "";
     let i = 0;
 
     const typeWriter = () => {
         if (i < text.length) {
-            element.textContent += text.charAt(i);
+            element.innerHTML += text.charAt(i);
             i++;
             setTimeout(typeWriter, speed);
         }
